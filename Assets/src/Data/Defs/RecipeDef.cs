b8e9f90ac6fd6e991a3c7b30be974ac3437bf4d2
djih <djimon@gmx.de>
2026-01-01 20:37:49 +0100

    updated recipeDef to overcome orphaned string id values

--- Assets/src/Data/Defs/RecipeDef.cs
+++ Assets/src/Data/Defs/RecipeDef.cs
@@ -1,6 +1,7 @@
 using CHAL.Data;
 using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace CHAL.Systems.Crafting
@@ -21,34 +22,46 @@ namespace CHAL.Systems.Crafting
         public GearType slotType;
 
         [Header("Kosten (Items)")]
-        public List<MaterialCost> inputs = new();
+        public List<MaterialCost> inputs;
 
         [Header("Kosten (Währung)")]
         public List<CurrencyCost> currencyCosts = new();
 
         [Header("Output")]
+        public ItemDef outputRef;
         public string outputItemId;    // z.B. "gear:chest_leather"
         [Min(1)] public int outputCount = 1;
 
         private void OnValidate()
         {
-            if (outputCount < 1) outputCount = 1;
-            if (inputs != null)
+            if(outputRef != null && outputRef.itemId != outputItemId)
+                outputItemId = outputRef.itemId;
+
+            for (int k = 0; k < inputs.Count; k++)
             {
-                for (int i = 0; i < inputs.Count; i++)
-                    if (inputs[i].qty < 1) inputs[i] = new MaterialCost { itemId = inputs[i].itemId, qty = 1 };
+                if (inputs[k].itemref != null && inputs[k].itemId != inputs[k].itemref.itemId)
+                {
+                    var id = inputs[k].itemref.itemId;
+                    int q = inputs[k].qty < 0 ? 0 : inputs[k].qty;
+                    inputs[k] = new MaterialCost { itemref = inputs[k].itemref, itemId = id, qty = q };
+                }
             }
+
+            if (outputCount < 1) outputCount = 1;
+
             if (currencyCosts != null)
             {
                 for (int i = 0; i < currencyCosts.Count; i++)
-                    if (currencyCosts[i].amount < 1) currencyCosts[i] = new CurrencyCost { currencyId = currencyCosts[i].currencyId, amount = 1 };
+                    if (currencyCosts[i].amount < 0) currencyCosts[i] = new CurrencyCost { currencyId = currencyCosts[i].currencyId, amount = 0};
             }
+
         }
     }
 
     [Serializable]
     public struct MaterialCost
     {
+        public ItemDef itemref;
         public string itemId;       // z.B. "part:iron_ingot"
         [Min(1)] public int qty;
     }
