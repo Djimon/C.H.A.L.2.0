c378dc6ab404f9f23ba1945c01898e028d3ca2ea
djih <djimon@gmx.de>
2026-01-29 19:45:22 +0100

    finetuned rarity Colors

--- Assets/src/Data/Enums/ItemRarity.cs
+++ Assets/src/Data/Enums/ItemRarity.cs
@@ -22,13 +22,14 @@ namespace CHAL.Data
         {
             { Rarity.unknown, Color.gray },
             { Rarity.Common, Color.white },
-            { Rarity.Uncommon, new Color(138f/255f, 165f/255f, 230f/255f) }, // Hellblau
-            { Rarity.Rare,     new Color(47f/255f, 181f/255f, 105f/255f) },  // Smaragdgrün
-            { Rarity.Epic,     new Color(217f/255f, 175f/255f, 61f/255f) },  // Gold
-            { Rarity.Legendary,new Color(110f/255f,  38f/255f, 212f/255f) }, // Violett
-            { Rarity.Mythic,   new Color(191f/255f,  63f/255f, 178f/255f) }, // Magenta
-            { Rarity.Holy,     new Color(172f/255f, 232f/255f, 217f/255f) }, // Türkis
+            { Rarity.Uncommon, new Color(108f/255f, 215f/255f, 145f/255f) }, // Hellgrün
+            { Rarity.Rare,     new Color(  0f/255f, 156f/255f, 230f/255f) }, // Cyan
+            { Rarity.Epic,     new Color(235f/255f, 160f/255f,  60f/255f) }, // Rot-Gold
+            { Rarity.Legendary,new Color(130f/255f,  70f/255f, 220f/255f) }, // Violett
+            { Rarity.Mythic,   new Color(210f/255f,  85f/255f, 160f/255f) }, // Magenta
+            { Rarity.Holy,     new Color(249f/255f, 232f/255f, 156f/255f) }, // Radiant-Gold
             { Rarity.Daemonic, new Color( 87f/255f,  12f/255f,  27f/255f) }  // Blutrot
+            // Uniques: 206,40,45
         };
   
 /// <summary>
